be87e09dea2083a4e9720f3f99a86e9861ba9dd0
Everton Pavan <everton.pavan.dev@gmail.com>
2024-03-14 08:51:08 -0300

    ci: add advertised hostname kafka as localhost

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -5,15 +5,13 @@ import { v4 as uuidv4 } from 'uuid';
 const OrderController= {
   create: async (req: Request, res: Response) => {
 
-    // const newOrder = {
-    //   orderId: Math.floor(Math.random() * 1000),
-    //   product: 'Product Name',
-    //   quantity: 2,
-    //   price: 20.99,
-    //   // Add more properties as needed
-    // };
-
-    const newOrder = "aa"
+    const newOrder = {
+      orderId: Math.floor(Math.random() * 1000),
+      product: 'Product Name',
+      quantity: 2,
+      price: 20.99,
+      // Add more properties as needed
+    };
 
     await createAndSendOrder(newOrder)
 
